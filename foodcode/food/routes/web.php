c0b15c4be63486bdf6fc782405092141e1772fbc
new name <new email>
2020-02-20 11:07:37 +0700

    first commit

---
+++ foodcode/food/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+//Backend
+Route::get('/admin','AdminController@index');
+Route::get('/dashboard','AdminController@show_dashboard');
+Route::get('/logout','AdminController@logout');
+Route::post('/admin-dashboard','AdminController@dashboard');
